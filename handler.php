7ed9bee0403fbb67bf33cdcadf0588302fb91e86
Vladislav_Foros <budzakvlad@gmail.com>
2016-07-28 17:49:44 +0300

    Вынес всю логику из index.php в этот файл, некоторый кусок кода в функцию занес.

--- handler.php
+++ handler.php
@@ -1,26 +1,38 @@
 <?php
-    include ("mysql.php");
-    include ("base.php");
+    include ("config.php");
 
     $positionFirstNews; //с какой по счету новости начинать вывод на экран
     $count_news_on_page = 4;
 
     //вытягиваем из БД данные
     $_db = new db_mysql(MYSQL_USER, MYSQL_PASSWORD, MYSQL_SERVER, MYSQL_DB);	//создаем новый объект, передавая в качестве аргумента константы
-    $countNews = $_db->sql("SELECT COUNT(*) FROM news");
+    $a = $_db->sql("SELECT COUNT(1) FROM news");
+    $countNews = @mysql_fetch_array( $a );
 
     if (isset($_GET['num']))
     {
-        $positionFirstNews = $_GET['num'];
+        if ($_GET['num'] > $countNews[0])
+        {
+            //$_GET['num'] = 0;
+            $positionFirstNews = 0;
+        }
+        else
+        {
+            if ($_GET['num'] < 0)
+            {
+                //$_GET['num'] = $countNews - $count_news_on_page;
+                $positionFirstNews = $countNews[0] - $count_news_on_page;
+            }
+            else
+            {
+                $positionFirstNews = $_GET['num'];
+            }
+        }
     }
     else
     {
         $positionFirstNews = 0;
     }
-
-$_db->close();
-
-
 
     function PrintNews($positionFirstNews, $count_news_on_page)
     {
@@ -36,4 +48,6 @@ $_db->close();
                                  ";
         }
     }
+
+$_db->close();
 ?>
