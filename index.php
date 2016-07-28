db9a7c62ef6cdf67164606313f92cd8a0c0fc647
Vladislav_Foros <budzakvlad@gmail.com>
2016-07-28 17:50:35 +0300

    Сделал только представление, вся логика вынесена в файл обработчик - handler.php

--- index.php
+++ index.php
@@ -10,20 +10,20 @@
         <div id = "content">
             <?php
                 include ("handler.php");
-                /*if (isset($_GET['num']))
-                {
-                    $positionFirstNews = $_GET['num'];
-                }
-                else
-                {
-                    $positionFirstNews = 0;
-            }*/
                 PrintNews($positionFirstNews, $count_news_on_page);
             ?>
         </div>
         <div id = "navigation">
-            <a href = "http://localhost/learning/index.php?num=<?php if (($positionFirstNews - $count_news_on_page) < 0 ) $positionFirstNews = 0; echo $positionFirstNews - $count_news_on_page;; ?>">Назад</a>
-            <a href = "http://localhost/learning/index.php?num=<?php if (($positionFirstNews - $count_news_on_page) > $count_news_on_page ) $positionFirstNews = 0; echo $positionFirstNews + $count_news_on_page;; ?> ">Далее</a>
+            <a href = "http://localhost/learning/index.php?num=<?php
+                echo $positionFirstNews - $count_news_on_page;
+                ?>">Назад
+            </a>
+
+            <a href = "http://localhost/learning/index.php?num=<?php
+                echo $positionFirstNews + $count_news_on_page;
+                ?>">Далее
+            </a>
+
         </div>
     </body>
 </html>
