2abd2dd91a494af7ce7e48422e258357e8c97db7
Vladislav_Foros <budzakvlad@gmail.com>
2016-07-29 17:22:50 +0300

    Собран, как контроллер, представление вынес в отдельный файл template.html

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
-    include ("handler.php");
+    include ("core.php");
+    include("newsModel.php");
+    include('template.html');
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf8">
-    </head>
-    <body>
-        <div id = "title">
-            <h1> Список новостей: </h1>
-        </div>
-        <div id = "content">
-            <?php
-                PrintNews($positionFirstNews, $count_news_on_page);
-            ?>
-        </div>
-        <div id = "navigation">
-            <a href = "http://localhost/learning/index.php?num=<?php
-                echo $positionFirstNews - $count_news_on_page;
-                ?>">Назад
-            </a>
 
-            <a href = "http://localhost/learning/index.php?num=<?php
-                echo $positionFirstNews + $count_news_on_page;
-                ?>">Далее
-            </a>
-
-        </div>
-    </body>
-</html>
