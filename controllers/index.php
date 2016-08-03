835f595b6db0a3b1a89e8cef9dd5788563fae496
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-03 17:57:50 +0300

    ООП подход

--- controllers/index.php
+++ controllers/index.php
@@ -2,5 +2,19 @@
     include '../core.php';
     include'../models/newsModel.php';
     include'../templates/template.php';
+
+    class Controller
+    {
+        private $model;
+
+        function __construct($model)
+        {
+            $this->model = $model;
+        }
+    }
+
+    $model = new Model();
+    $controller = new Controller($model);
+    $view = new View($controller, $model);
 ?>
 
