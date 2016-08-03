5cf4fe35802eff386fbc7ad723f234778271d5c2
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-03 17:58:02 +0300

    ООП подход

---
+++ views/newsView.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 03.08.2016
+ * Time: 16:48
+ */
+    class View
+    {
+        private $model;
+        private $controller;
+        private $mainPath;
+
+        function __construct($controller, $model)
+        {
+            $this->controller = $controller;
+            $this->model = $model;
+            $this->mainPath = "localhost/learning/";
+        }
+
+        function ShowTemplate($path)
+        {
+            include_once "$this->mainPath.$path";
+        }
+
+        function ShowNews($arrayNews)
+        {
+            
+
+
+        }
+    }
+?>
