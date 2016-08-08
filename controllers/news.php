4cd930cd88f20c1257afedad92cb9f41645c71cb
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-08 20:49:28 +0300

    поисправлял названия функций.

--- controllers/news.php
+++ controllers/news.php
@@ -1,8 +1,8 @@
 <?php
     include '../core.php';
     include'../models/newsModel.php';
-    sqlInjection($_db);
-    SetPosition($countNews, $count_news_on_page, $positionFirstNews);
+    $countNews = SetCountNews($_db);
+    $positionFirstNews = SetPosition($countNews, $count_news_on_page, $_GET['num']);
     $arr = GetNews($positionFirstNews, $count_news_on_page);
     include'../templates/template.php';
 ?>
