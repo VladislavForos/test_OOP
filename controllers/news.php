0e8b940194e4b808f40704a419acd96910b93028
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-08 20:23:10 +0300

    Явно вызываю функции в контроллере.

--- controllers/news.php
+++ controllers/news.php
@@ -1,9 +1,8 @@
 <?php
     include '../core.php';
     include'../models/newsModel.php';
+    sqlInjection($_db);
+    SetPosition($countNews, $count_news_on_page, $positionFirstNews);
     $arr = GetNews($positionFirstNews, $count_news_on_page);
     include'../templates/template.php';
-
-
 ?>
-
