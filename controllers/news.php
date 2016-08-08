de9a652b6cd04bc41197fd904baac20e23737e07
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-08 21:43:36 +0300

    передаю в качестве агрумента переменную $_db.

--- controllers/news.php
+++ controllers/news.php
@@ -3,6 +3,6 @@
     include'../models/newsModel.php';
     $countNews = SetCountNews($_db);
     $positionFirstNews = SetPosition($countNews, $count_news_on_page, $_GET['num']);
-    $arr = GetNews($positionFirstNews, $count_news_on_page);
+    $arr = GetNews($positionFirstNews, $count_news_on_page, $_db);
     include'../templates/template.php';
 ?>
