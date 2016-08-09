858293376797444dc69148ab17ee892c24418afa
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-09 18:50:17 +0300

    старый "template.php"

---
+++ templates/newsTemplate.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+</head>
+<body>
+<div id = "title">
+    <h1> Список новостей: </h1>
+</div>
+<div id = "content">
+    <?php
+                //вывод новостей на экран
+                foreach ($arr as $elem)
+                {
+                    echo "  <h3> " . @$elem['title'] . " </h3>
+                     <p> " . @$elem['content'] . " </p>
+                    <br>
+                     ";
+            }
+            ?>
+</div>
+<div id = "navigation">
+    <a href = "http://localhost/testing/controllers/index.php?num=<?php
+                echo $positionFirstNews - $count_news_on_page;
+                ?>">Назад
+    </a>
+
+    <a href = "http://localhost/testing/controllers/index.php?num=<?php
+                echo $positionFirstNews + $count_news_on_page;
+                ?>">Далее
+    </a>
+
+</div>
+</body>
+</html>
