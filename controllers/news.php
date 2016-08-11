b81365d3d5f1182a201fb156b273cbda6c34495f
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-11 14:25:16 +0300

    удалил все из главной ветки

--- controllers/news.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include '../core.php';
-    include'../models/newsModel.php';
-    $countNews = SetCountNews($_db);
-    $positionFirstNews = SetPosition($countNews, $count_news_on_page, $_GET['num']);
-    $arr = GetNews($positionFirstNews, $count_news_on_page, $_db);
-    include '../templates/newsTemplate.php';
-?>
--- core.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include '../lib/mysql.php';
-    include '../lib/config.php';
-
-    $_db = new db_mysql(MYSQL_USER, MYSQL_PASSWORD, MYSQL_SERVER, MYSQL_DB);	//создаем новый объект, передавая в качестве аргумента константы
-
-?>
