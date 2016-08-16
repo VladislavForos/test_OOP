dcf911cf0bdc47b11d28eadae0dd307f19ca968d
Vladislav_Foros <budzakvlad@gmail.com>
2016-08-16 19:43:45 +0300

    переделал логику на вывод новостей с параметром: номер страницы, а не номер новости, с какой начинать вывод.

--- models/newsModel.php
+++ models/newsModel.php
@@ -1,52 +1,57 @@
 <?php
+define("firstPage",1);
     class NewsModel
     {
-        public $positionFirstNews; //с какой по счету новости начинать вывод на экран
+        public $pageNumber;
         public $count_news_on_page = 4;
         private $countNews;
+        private $maxPage;
         public $newsArray;
         private $_db;
 
         function __construct($_db)
         {
-            $this->positionFirstNews = 0;
+            $this->pageNumber = 1;
             $this->_db = $_db;
             $this->_db->sql("SELECT COUNT(*) FROM news");
             $temp =$this->_db->line();
             $this->countNews = $temp['COUNT(*)'];
+            $this->maxPage = ceil($this->countNews/$this->count_news_on_page);
+            settype($this->maxPage, 'integer');
         }
 
-        function setPositionFirstNew($get)
+        function checkNumberPage($digit)
         {
-            if (isset($get))
+            if (settype($digit,'integer'))
             {
-                if ($get >= $this->countNews)
+                if ($digit > $this->maxPage)
                 {
-                    $this->positionFirstNews = 0;
+                    return firstPage;
                 }
                 else
                 {
-                    if ($get < 0)
+                    if ($digit < 1)
                     {
-                        $this->positionFirstNews = $this->countNews - $this->count_news_on_page;
+                        return $this->maxPage;
                     }
                     else
                     {
-                        $this->positionFirstNews = $get;
+                        return $digit;
                     }
                 }
             }
             else
             {
-                $this->positionFirstNews = 0;
+                return firstPage;
             }
         }
 
-        function getNews($numberPage)
+        function getNews($page)
         {
-            $this->_db->sql("SELECT * FROM news WHERE id > ".$this->positionFirstNews." ORDER BY id ASC LIMIT ".$this->count_news_on_page);
+            $this->pageNumber = $this->checkNumberPage($page);
+            $positionFirstNew = ($this->pageNumber-1)*$this->count_news_on_page + 1;
+            $this->_db->sql("SELECT * FROM news WHERE id >= ".$positionFirstNew." ORDER BY id ASC LIMIT ".$this->count_news_on_page);
             $news = $this->_db->matr();	//записываем в ассоциативный массив все записи из таблицы 'news'
             return $news;
         }
-
     }
